8e220db46f4121cd2c45ec95d108eb438f9646b8
Lincon Avila <lincon1000ls@gmail.com>
2025-05-29 22:46:23 -0300

    Add LotteryScheduler class (incomplete)

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
-#include <time.h>
 #include <vector>
 
 class Process {
     public:
-        std::string pid;
+        int pid;
         int creation_time;
         int burst_time;
         int remaining_time;
@@ -15,21 +14,45 @@ class Process {
         int tickets;
         int weights;
 
-        Process(std::string pid, int creation_time, int burst_time, int tickets= 0) {
+        Process(int pid, int creation_time, int burst_time, int tickets= 0) {
             this->pid = pid;
             this->creation_time = creation_time;
             this->burst_time = burst_time;
+            this->remaining_time = burst_time;
+            this->start_time = -1;
+            this->end_time = -1;
+            this->ready_time = creation_time;
             this->tickets = tickets;
             this->weights = tickets;
     }
 };
 
+class LotteryScheduler {
+    public:
+        int num_processes;
+        std::vector<Process> processes;
+        std::vector<Process> ready_queue;
+        int quantum;
+        int total_tickets;
+        int current_time = 0;
 
 
-
-
+    LotteryScheduler(int num_processes, int quantum, int total_tickets = 0) {
+        this->num_processes = num_processes;
+        this->quantum = quantum;
+        this->total_tickets = total_tickets;
+        this->processes.reserve(num_processes);
+        this->ready_queue.reserve(num_processes);
+    }
 
     
+
+
+
+    void add_process(const Process& process) {
+        processes.push_back(process);
+    }
+};
         
 
 
